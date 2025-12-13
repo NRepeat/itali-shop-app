4dafdf6b844d38772cd67d6e443a6307071b6947
Nikita <nnazarov55@gmail.com>
2025-12-13 16:51:32 +0200

    create createAttributes

---
+++ app/service/create-attributes.ts
@@ -0,0 +1,31 @@
+import { externalDB } from "@shared/lib/prisma/prisma.server";
+import { getOcFilterMap } from "./maps/metafields";
+import { createMetaobject } from "./shopify/metaobjects/createMetaobject";
+import { CreateMetaobjectMutationVariables, MetaobjectStatus } from "@/types";
+import { AdminApiContext } from "@shopify/shopify-app-react-router/server";
+
+export const createAttributes = async (productId:number, admin: AdminApiContext) => {
+
+  const ids = [ ]
+
+ const product_attributes = await externalDB.bc_product_attribute.findMany({where:{product_id:productId,language_id:3}})
+ if(product_attributes && product_attributes.length > 0){
+   for(const attr of product_attributes){
+    const attribute =  await externalDB.bc_attribute.findFirst({where:{attribute_id:attr.attribute_id}})
+    if(!attribute) continue;
+    const attributeDesc = await externalDB.bc_attribute_description.findFirst({where:{attribute_id:attribute.attribute_id,language_id:3}})
+    if(!attributeDesc) continue;
+    const input:CreateMetaobjectMutationVariables = {
+      metaobject:{
+        type:"attribute",
+        capabilities:{
+          publishable:{status:"ACTIVE" as MetaobjectStatus.Active}
+        },
+        fields:[{key:"title",value:attributeDesc?.name},{key:'atribute_payload',value:attr.text}]
+      }
+    }
+    await createMetaobject(input,admin)
+
+   }
+ }
+};
